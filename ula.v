524fe06dd3691b8c8e3d98811075fe486ebe0930
Giovanna Costa <162203938+giouvann@users.noreply.github.com>
2026-05-27 17:23:15 -0300

    Create ula.v

---
+++ ula.v
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////////
+// GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro
+// ATIVIDADE: Projeto 02 - 2ª VA
+// DISCIPLINA: Arquitetura e Organização de Computadores
+// SEMESTRE: 2026.1
+// DESCRIÇÃO: Unidade Lógica e Aritmética (ULA).
+//            Recebe dois operandos de 32 bits e um código de operação de 4 bits
+//            vindo da ula_ctrl. Realiza operações aritméticas, lógicas e de
+//            deslocamento necessárias para executar todas as instruções do projeto.
+////////////////////////////////////////////////////////////////////////////////////
