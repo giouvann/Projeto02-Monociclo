cbc65a8ecd5ece190290266f1ab2965fd67d6db0
Giovanna Costa <162203938+giouvann@users.noreply.github.com>
2026-05-27 18:38:32 -0300

    Update top_level.v

--- top_level.v
+++ top_level.v
@@ -1,1 +1,10 @@
-
+////////////////////////////////////////////////////////////////////////////////////
+// GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro
+// ATIVIDADE: Projeto 02 - 2ª VA
+// DISCIPLINA: Arquitetura e Organização de Computadores
+// SEMESTRE: 2026.1
+// ARQUIVO: Módulo top-level do processador MIPS monociclo.
+// DESCRIÇÃO: Integra todos os módulos: PC, i_mem, regfile, ULA, ula_ctrl,
+//            ctrl e d_mem. Implementa os multiplexadores e conexões
+//            necessárias para executar as instruções do subconjunto              
+////////////////////////////////////////////////////////////////////////////////////
