17a20c2aec152c9b1f1728beb6cc7da38cb727e1
David Melo <dev.davmelo@gmail.com>
2026-05-31 21:11:39 -0300

    feat: implementa multiplexador 2x1 de 32 bits

---
+++ mux2x1_32bits.v
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////////
+// GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro
+// ATIVIDADE: Projeto 02 - 2ª VA
+// DISCIPLINA: Arquitetura e Organização de Computadores
+// SEMESTRE: 2026.1
+// ARQUIVO: Multiplexador de 2 para 1 com 5 bits (mux2x1_5bits)
+// DESCRIÇÃO:           
+////////////////////////////////////////////////////////////////////////////////////
+
+module mux2x1_5bits(
+   input [31:0]A,                  // primero barramento de entrada de 32 bits
+   input [31:0]B,                  // segundo barramento de entrada de 32 bits
+   input S,                        // sinal de seleção do multiplexador
+   output [31:0]Y                  // barramento de saída de 32 bits
+);
+   assign Y = (S == 0) ?           // seleciona qual entrada será encaminhada para a saída
+   A                               // quando S = 0, a saída recebe o valor da entrada A
+   :
+   B;                              // quando S = 1, a saída recebe o valor da entrada B
+endmodule
